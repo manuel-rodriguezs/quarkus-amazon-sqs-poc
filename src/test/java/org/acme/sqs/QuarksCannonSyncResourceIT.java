733fc34f28490f67c5768bc3913bcc52cfbb7df2
Manuel Rodríguez Sánchez <manuel@tulotero.com>
2022-04-13 11:44:59 +0200

    Initial

---
+++ src/test/java/org/acme/sqs/QuarksCannonSyncResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.sqs;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class QuarksCannonSyncResourceIT extends QuarksCannonSyncResourceTest {
+
+    // Execute the same tests but in native mode.
+}
